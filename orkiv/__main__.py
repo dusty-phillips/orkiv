6a9a815f29e3913bb4cf38125e21416ad3c7770d
Dusty Phillips <dusty@archlinux.ca>
2013-06-12 22:48:02 -0600

    Example 8: Using an AnchorLayout to stick the form to the top of the window.

--- orkiv/__main__.py
+++ orkiv/__main__.py
@@ -1,8 +1,8 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 
 
-class AccountDetailsForm(BoxLayout):
+class AccountDetailsForm(AnchorLayout):
     pass
 
 
