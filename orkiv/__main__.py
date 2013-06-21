b6f8f6bb51cd8184ff28613b0c66186dd9c77218
Dusty Phillips <dusty@archlinux.ca>
2013-06-20 21:55:49 -0600

    Example 13: Hook up some ObjectProperty objects so we can access widgets from inside the class.
    Still no change in functionality, but everything is in place to be able to access the values in our event handler in the next example.

--- orkiv/__main__.py
+++ orkiv/__main__.py
@@ -1,8 +1,13 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.properties import ObjectProperty
 
 
 class AccountDetailsForm(AnchorLayout):
+    server_box = ObjectProperty()
+    username_box = ObjectProperty()
+    password_box = ObjectProperty()
+
     def login(self):
         print("Click the goddamn button")
 
