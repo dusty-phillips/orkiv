b5c3a9ad3196b7219bbbde148e650135e52bedfe
Dusty Phillips <dusty@archlinux.ca>
2013-06-21 21:01:55 -0600

    Example 17: Connect to jabber and print buddy list on login.
    Disconnects immediately so as not to keep the program open when the window is closed.

--- orkiv/__main__.py
+++ orkiv/__main__.py
@@ -10,9 +10,13 @@ class AccountDetailsForm(AnchorLayout):
     password_box = ObjectProperty()
 
     def login(self):
-        print(self.server_box.text)
-        print(self.username_box.text)
-        print(self.password_box.text)
+        jabber_id = self.username_box.text + "@" + self.server_box.text
+        password = self.password_box.text
+
+        app = Orkiv.get_running_app()
+        app.connect_to_jabber(jabber_id, password)
+        print(app.xmpp.client_roster.keys())
+        app.xmpp.disconnect()
 
 
 class Orkiv(App):
